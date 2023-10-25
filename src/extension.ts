d25747e9c01bf4b6c405780b768c0f1f06f9a2c8
Yahya Batulu <yahyabatulu00@gmail.com>
2023-10-25 22:03:08 +0300

    Types added

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,11 @@
 import * as vscode from 'vscode';
 import MarkdownIt from 'markdown-it';
+import alertPlugin from 'markdown-it-github-alert';
 
 export function activate(context: vscode.ExtensionContext) {
 	return {
         extendMarkdownIt(md: MarkdownIt) {
-            return md.use(emoji);
+            return md.use(alertPlugin);
         }
     };
 }
