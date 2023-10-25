7ee28d464d1f8a91c3181c0d1bbbc440b943a452
Yahya Batulu <yahyabatulu00@gmail.com>
2023-10-25 22:50:23 +0300

    Push before sleep

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,23 @@
 import * as vscode from 'vscode';
 import MarkdownIt from 'markdown-it';
-import alertPlugin from 'markdown-it-github-alert';
+import { alertPlugin } from 'markdown-it-github-alert';
 
 export function activate(context: vscode.ExtensionContext) {
-	return {
+	let orange = vscode.window.createOutputChannel("Pikod");
+    orange.appendLine("Pikod Markdown Extension is now active!");
+    vscode.window.showInformationMessage('Pikod Markdown Extension is now active!');
+    return {
+        
         extendMarkdownIt(md: MarkdownIt) {
+        
+            md.use(function (md) {
+                orange.appendLine("Pikod deneme");
+            });
             return md.use(alertPlugin);
         }
     };
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    vscode.window.showInformationMessage('Pikod Markdown Extension is now deactive!');
+}
