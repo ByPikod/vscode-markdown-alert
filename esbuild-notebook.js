b7997cc231fc69263a285d560a396cf13baf93c4
Yahya Batulu <46069238+ByPikod@users.noreply.github.com>
2023-10-26 14:00:04 +0300

    Build changed

---
+++ esbuild-notebook.js
@@ -0,0 +1,40 @@
+const esbuild = require('esbuild');
+const path = require('path');
+
+const args = process.argv.slice(2);
+
+const isWatch = args.indexOf('--watch') >= 0;
+
+let outputRoot = __dirname;
+const outputRootIndex = args.indexOf('--outputRoot');
+if (outputRootIndex >= 0) {
+	outputRoot = args[outputRootIndex + 1];
+}
+
+const srcDir = path.join(__dirname, 'notebook');
+const outDir = path.join(outputRoot, 'dist-notebook');
+
+async function build() {
+	await esbuild.build({
+		entryPoints: [
+			path.join(srcDir, 'index.ts'),
+		],
+		bundle: true,
+		minify: true,
+		sourcemap: false,
+		format: 'esm',
+		outdir: outDir,
+		platform: 'browser',
+		target: ['es2020'],
+	});
+}
+
+
+build().catch(() => process.exit(1));
+
+if (isWatch) {
+	const watcher = require('@parcel/watcher');
+	watcher.subscribe(srcDir, () => {
+		return build();
+	});
+}
